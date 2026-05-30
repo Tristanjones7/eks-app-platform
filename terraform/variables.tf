d08d09cca18d713f0a6b2f16aecc8d68177cac36
TJones <tristanjones77@outlook.com>
2026-05-30 05:15:57 +0000

    Add EKS cluster Terraform (VPC, EKS, managed node group)

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "project_name" {
+  type    = string
+  default = "eks-app-platform"
+}
+variable "cluster_version" {
+  type    = string
+  default = "1.30"
+}
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+variable "node_instance_type" {
+  type    = string
+  default = "t3.medium"
+}
+variable "node_desired_size" {
+  type    = number
+  default = 2
+}
+variable "node_min_size" {
+  type    = number
+  default = 1
+}
+variable "node_max_size" {
+  type    = number
+  default = 3
+}
